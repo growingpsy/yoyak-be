90812066bbb9f253dcd1c150ad4b373c89099868
sangjiii <jsangjii@gmail.com>
2025-02-28 19:42:15 +1300

    ✨ [UPDATE]: Swagger JSON 제거하고 연결 방식 수정

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-dotenv.config(); 
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const domain = process.env.APP_DOMAIN || 'http://localhost';
   const port = process.env.PORT || 3000;
-  await app.listen(port);
   console.log(`${port}번 포트에서 대기 중`);
+
+  app.use(cors({
+    origin: ['http://localhost:3000', 'https://yoyaklery.site'],
+    methods: 'GET, POST, DELETE, PATCH, PUT, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  }));
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Yoyaklary API') 
+    .setDescription('Yoyaklary의 NestJS API 문서')  
+    .setVersion('1.0')  
+    .addBearerAuth(  
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',  
+        name: 'Authorization',
+        description: 'JWT 토큰을 입력하세요',
+        in: 'header',
+      },
+      'access-token',  
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('yoyaklary', app, document); 
+
+
+  await app.listen(port, () => {
+    console.log(`Application is running on: ${domain}:${port}`);
+  });
 }
 bootstrap();
