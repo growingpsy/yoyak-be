e160cf70517520c8aca60a27e9076d1d748d3897
sangjiii <jsangjii@gmail.com>
2025-03-18 22:32:10 +1300

    ♻️ [refactor]: review.dto에 유효성 검사 추가 및 UpdateReviewDto 수정정

--- src/layer/dtos/review.dto.ts
+++ src/layer/dtos/review.dto.ts
@@ -1,19 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateReviewDto {
   @ApiProperty({ example: 1, description: '연관된 컨텐츠 ID' })
+  @IsInt({ message: 'content_id는 정수여야 합니다.' })
   readonly content_id!: number;
 
   @ApiProperty({ example: '이 영화 정말 감동적이었어요!', description: '리뷰 내용' })
+  @IsString({ message: 'review_text는 문자열이어야 합니다.' })
+  @IsNotEmpty({ message: 'review_text는 비워둘 수 없습니다.' })
   readonly review_text!: string;
 }
 
 export class UpdateReviewDto {
   @ApiProperty({ example: '업데이트된 리뷰 내용', description: '변경할 리뷰 텍스트', required: false })
-  readonly review_text?: string;
-
-  @ApiProperty({ example: new Date().toISOString(), description: '수정된 날짜', required: true })
-  updated_at!: Date;
+  @IsString({ message: 'review_text는 문자열이어야 합니다.' })
+  @IsOptional()
+  review_text?: string;
 }
 
 export class ReviewResponseDto {
@@ -28,4 +31,7 @@ export class ReviewResponseDto {
 
   @ApiProperty({ example: new Date().toISOString(), description: '리뷰 생성 날짜' })
   created_at!: Date;
+
+  @ApiProperty({ example: new Date().toISOString(), description: '리뷰 수정 날짜' })
+  updated_at!: Date;
 }
